30785617e0545f07d16e3198afbd347566315639
James Vaughan <james@recx.co.uk>
2020-10-08 11:12:37 +0100

    Updated to include extra rules, plugins and development options

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  mode: 'development',
+  entry: {
+    app: './src/app.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
-    new CleanWebpackPlugin(),
+    // new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
     new HtmlWebpackPlugin({
       title: 'Frontend | jql',
     }),
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -23,6 +33,13 @@ module.exports = {
           { loader: 'style-loader', options: { injectType: 'linkTag' } },
           { loader: 'file-loader' },
         ],
+      },
+      {
+        test: /\.(png|ico|webmanifest)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
       },
       {
         test: /\.css.map$/,
